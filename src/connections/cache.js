e17c92d2c45209006666bba4d8f93f523f22c0cf
brettsodie <blsodie@gmail.com>
2020-10-31 12:14:00 -0400

    feat: redis config

--- src/connections/cache.js
+++ src/connections/cache.js
@@ -1,7 +1,9 @@
 const redis = require('redis');
 
 const publisher = redis.createClient({
-  port: 6379,              
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,   
+  password: process.env.REDIS_PASSWORD,           
 });
 
 module.exports = publisher;
