efd74a06057c35778646f9c65865e2626f3f87f2
brettsodie <blsodie@gmail.com>
2020-11-11 19:41:29 -0500

    feat: add speaker service path method

--- src/services/consumer/speaker/controllers/index.js
+++ src/services/consumer/speaker/controllers/index.js
@@ -1,7 +1,9 @@
 const create = require('./create');
 const get = require('./get');
+const patch = require('./patch');
 
 module.exports = [
   create,
   get,
+  patch,
 ];
---
+++ src/services/consumer/speaker/controllers/patch.js
@@ -0,0 +1,33 @@
+const handlers = require('@/utilities/handlers');
+const throwError = require('@/utilities/throwError');
+const userAuth = require('@/utilities/userAuth');
+const Speaker = require('@/services/consumer/speaker/model');
+const redis = require('@/connections/cache');
+
+const controller = async (req, res) => {
+  try {
+    const { _id } = userAuth(req.header('authorization'));
+    const speaker = await Speaker.findOne({ user_id: _id })
+    if (!speaker) throwError(404, 'no speaker found');
+
+    speaker.muted = true;
+
+    Object.keys(req.body).forEach(k => {
+      speaker[k] = req.body[k]
+    });
+
+    const updatedSpeaker = await speaker.save();
+
+    redis.publish('mute', _id.toString());
+
+    handlers.success(res, 200, updatedSpeaker);
+  } catch(e) {
+    handlers.error(res, e, '/consumer/speaker PATCH');
+  }
+};
+
+module.exports = {
+  controller,
+  method: 'patch',
+  path: '/speaker',
+};
